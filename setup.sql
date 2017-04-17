262abc663e5017f38772e25c74668ba31b9dabc4
Sammmpo <sampsa.kares@pp.inet.fi>
2017-04-17 14:50:33 +0300

    update

---
+++ setup.sql
@@ -0,0 +1,14 @@
+/* Import this script into phpMyAdmin to setup everything. */
+/* Use this after running the application at least once. */
+
+INSERT INTO question (id, statement, answer) VALUES (1, "1+1=3", false);
+INSERT INTO question (id, statement, answer) VALUES (2, "2+8=10", true);
+INSERT INTO question (id, statement, answer) VALUES (3, "1+1=2", true);
+INSERT INTO question (id, statement, answer) VALUES (4, "10+14=25", false);
+INSERT INTO question (id, statement, answer) VALUES (5, "9+7=18", false);
+
+INSERT INTO question (id, statement, answer) VALUES (6, "3+10=33", false);
+INSERT INTO question (id, statement, answer) VALUES (7, "10+1=21", false);
+INSERT INTO question (id, statement, answer) VALUES (8, "10-4=4", true);
+INSERT INTO question (id, statement, answer) VALUES (9, "2*3=6", true);
+INSERT INTO question (id, statement, answer) VALUES (10, "10*100=10000", false);
